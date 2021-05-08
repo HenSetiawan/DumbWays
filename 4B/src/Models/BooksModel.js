cd5ef898c2c680e6341871199e39a4f030a5402e
henSetiawan <hendystyawan74@gmail.com>
2021-05-08 15:40:06 +0700

    create books model

---
+++ 4B/src/Models/BooksModel.js
@@ -0,0 +1,35 @@
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize('dumbways', 'root', '', {
+    host: 'localhost',
+    dialect:'mysql'
+  });
+
+  const books=sequelize.define('books',{
+    'id':{
+        type:Sequelize.DataTypes.INTEGER,
+        primaryKey:true
+    },
+    'name':{
+        type:Sequelize.DataTypes.STRING
+    },
+    'stok':{
+        type:Sequelize.DataTypes.INTEGER
+    },
+    'image':{
+        type:Sequelize.DataTypes.STRING
+    },
+    'deskripsi':{
+        type:Sequelize.DataTypes.STRING
+    },
+    'category_id':{
+        type:Sequelize.DataTypes.STRING
+    },
+
+},{
+    timestamps:false
+}
+)
+
+module.exports=books;
