c330a5498688cd14a7b6426f18977e96c50efc0a
henSetiawan <hendystyawan74@gmail.com>
2021-05-08 16:07:29 +0700

    create get book by id endpoint

--- 4B/src/Controller/BooksController.js
+++ 4B/src/Controller/BooksController.js
@@ -9,3 +9,14 @@ exports.getAllBooks = async (req, res) => {
     res.json({ status: "500" }).status(500);
   }
 };
+
+exports.getBookById = async (req, res) => {
+ const id=req.params.id;
+  try {
+    const book = await booksModel.findByPk(id);
+    res.json(book);
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "500" }).status(500);
+  }
+};
--- 4B/src/Router/BooksRouter.js
+++ 4B/src/Router/BooksRouter.js
@@ -3,5 +3,5 @@ const booksRouter = express.Router();
 const BooksController = require("../Controller/BooksController");
 
 booksRouter.get("/v1/books", BooksController.getAllBooks);
-
+booksRouter.get("/v1/books/:id", BooksController.getBookById);
 module.exports = booksRouter;
